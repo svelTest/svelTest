c7541eb612d14d051ea80817e31a66ba8b78504e
Kaitlin Huben <kaitlin.huben@gmail.com>
2014-03-08 14:21:00 -0500

    Added Java file to test
    Compile with:
    	javac Add.java
    Run with:
    	java Add <number> <number>

---
+++ test/Add.java
@@ -0,0 +1,15 @@
+public class Add {
+	public static void main(String[] args) {
+		int z = add(1, 2);
+		System.out.println("The sum of 1 and 2 is " + z);
+
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = add(a, b);
+		System.out.println("Your sum is " + c);
+	}
+
+	public static int add(int x, int y) {
+		return x+y;
+	}
+}
