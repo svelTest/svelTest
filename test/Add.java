4f72eabd4e8336067aa83ad826318d5785f8657e
Kaitlin Huben <kaitlinhuben@users.noreply.github.com>
2014-03-08 14:47:34 -0500

    Remove args add for testing

--- test/Add.java
+++ test/Add.java
@@ -3,10 +3,10 @@ public class Add {
 		int z = add(1, 2);
 		System.out.println("The sum of 1 and 2 is " + z);
 
-		int a = Integer.parseInt(args[0]);
+		/*int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 		int c = add(a, b);
-		System.out.println("Your sum is " + c);
+		System.out.println("Your sum is " + c);*/
 	}
 
 	public static int add(int x, int y) {
