7e525b29835dc6f443eb381daba29b58bac448dd
Christopher So <christopherso92@gmail.com>
2014-03-22 16:15:55 -0400

    added Vagrantfile and provisioning file

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+
+sudo apt-get install -y python
+sudo apt-get install -y openjdk-6-jre-headless
+sudo apt-get install -y python-ply
+
+sudo apt-get install -y flex
+sudo apt-get install -y bison
+
+sudo apt-get install -y git
+sudo apt-get install -y vim
+
+echo svelTestVM: finished provisioning
