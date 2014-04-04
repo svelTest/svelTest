5821153d390ff73e25f4ababe53c8a90c4961bcf
emily <emily.j.hsia@gmail.com>
2014-04-04 15:52:51 -0400

    add Node class file

---
+++ lib/node.py
@@ -0,0 +1,26 @@
+class Node(object):
+
+	def __init__(self, type, children=None, leaf=None, token=None):
+		self.type = type
+		if children:
+			self.children = children
+		else:
+			self.children = []
+		self.leaf = leaf
+		self.token = token
+
+	def traverse(self, i):
+		s = self.type
+		indent = "\n" + i*' |'
+		if self.leaf != None:
+			if isinstance(self.leaf, Node):
+				print "Node"
+				s += indent + self.leaf.traverse(i+1)
+			else:
+				s += indent + str(self.leaf)
+		for children in self.children:
+			s += indent + children.traverse(i+1)
+		return s
+
+	def __str__(self):
+		return self.traverse(1)
