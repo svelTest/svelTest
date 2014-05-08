85da161a0a3f2b53d2f37f50970189cd5084225f
Josh Lieberman <josh.lieberman92@gmail.com>
2014-05-07 21:28:51 -0400

    added tutorial tests, fixed hello.svel test.

---
+++ tutorial/Add.java
@@ -0,0 +1,10 @@
+public class Add {
+	public static void main(String[] args) {
+		int z = add(1, 2);
+		System.out.println("The sum of 1 and 2 is " + z);
+	}
+
+	public static int add(int x, int y) {
+		return x + y;
+	}
+}
---
+++ tutorial/Hello.java
@@ -0,0 +1,7 @@
+//prints ‘Hello World!’ to console
+public class Hello {
+	public static void main (String args[]) {
+		String hello = “Hello World!”;
+		System.out.println(hello);
+	}
+}
